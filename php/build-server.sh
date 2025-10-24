28ec36a0c907960134577519b5359f63572dcbed
Boris Rybalkin <ribalkin@gmail.com>
2025-10-24 21:59:48 +0100

    download

--- php/build-server.sh
+++ php/build-server.sh
@@ -4,40 +4,29 @@ cd ${DIR}
 
 VERSION=$1
 BUILD_DIR=${DIR}/../build/snap
-apt update
-apt install -y \
-  zip \
-  wget \
-  unzip \
-  --no-install-recommends
+#apt update
+#apt install -y \
+#  zip \
+#  wget \
+#  unzip \
+#  --no-install-recommends
 
 cd ${DIR}/../build
 wget https://getcomposer.org/installer -O - -q | php -- --quiet
 mv composer.phar /usr/local/bin/composer
 
-wget --progress=dot:giga https://github.com/kimai/kimai/archive/refs/tags/2.40.0.tar.gz
-tar xf 2.40.0.tar.gz
-mv kimai-5-stable $BUILD_DIR/server
+mkdir $BUILD_DIR/server
+wget --progress=dot:giga https://github.com/kimai/kimai/archive/refs/tags/2.40.0.tar.gz -O kimai.tar.gz
+tar xf kimai.tar.gz --strip-components=1 -C $BUILD_DIR/server
 
 cd $BUILD_DIR/server
-composer config --global github-oauth.github.com $GITHUB_TOKEN
+#composer config --global github-oauth.github.com $GITHUB_TOKEN
 composer install --no-dev
 
 SNAP=/snap/kimai/current
 SNAP_DATA=/var$SNAP
 mkdir -p bin
 cp -r $DIR/bin/* bin
-sed -i "s/'driver' => '.*'/'driver' => 'syslog'/g" config/logging.php
-grep driver config/logging.php
-sed -i "s#'host'.*REDIS_HOST.*#'path'=>env('REDIS_PATH'),#g" config/database.php
-grep REDIS_PATH config/database.php
-sed -i "s#'port'.*REDIS_PORT.*#'scheme'=>'unix',#g" config/database.php
-grep scheme config/database.php
-sed -i "s#'root' => base_path().*#'root' => '$SNAP_DATA/storage',#g" config/filesystems.php
-grep root config/filesystems.php
-sed -i "s#return \$app;#\$app->useStoragePath( '$SNAP_DATA/storage' ); return \$app;#g" bootstrap/app.php
-grep return bootstrap/app.php
 
 rm -rf .env
 ln -s $SNAP_DATA/config/.env .env
-nv .env
