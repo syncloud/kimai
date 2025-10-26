6f026ed85b80d1184fac57b7b2fa52d85eb47d29
Boris Rybalkin <ribalkin@gmail.com>
2025-10-26 21:19:00 +0000

    mem

--- bin/console.sh
+++ bin/console.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 DIR=$( cd "$( dirname "$0" )" && cd .. && pwd )
 cd $DIR/server
+export memory_limit=512M
+export APP_ENV=prod
 export PATH=$PATH:$DIR/bin
 ${DIR}/bin/php.sh bin/console "$@"
