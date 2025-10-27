e947c283596d0ffdc1ef36f9f8dcea550a964188
Boris Rybalkin <ribalkin@gmail.com>
2025-10-27 22:37:45 +0000

    try ld vars

--- php/bin/php.sh
+++ php/bin/php.sh
@@ -6,6 +6,8 @@ LIBS=$LIBS:$(echo ${DIR}/usr/lib)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*/samba)
 MAGICK_CODER_MODULE_PATH=$(echo ${DIR}/usr/lib/ImageMagickCoders) \
   PHP_INI_SCAN_DIR=${DIR}/usr/local/etc/php:${DIR}/usr/local/etc/php/conf.d \
+  COMPOSER_MEMORY_LIMIT=-1 \
+  memory_limit=512M \
   ${DIR}/lib/*-linux*/ld-*.so \
   --library-path $LIBS \
   ${DIR}/usr/local/bin/php "$@"
