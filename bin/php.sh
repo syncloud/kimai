c2b62be65834b93d30a9a2398b48281cb69f3c17
Boris Rybalkin <ribalkin@gmail.com>
2025-10-27 22:35:55 +0000

    fix export

--- bin/php.sh
+++ bin/php.sh
@@ -2,5 +2,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export COMPOSER_MEMORY_LIMIT=-1
 export COMPOSER_ALLOW_SUPERUSER=1
-expoet memory_limit=512M
+export memory_limit=512M
 exec $DIR/php/bin/php.sh -c /snap/kimai/current/config/php.ini "$@"
