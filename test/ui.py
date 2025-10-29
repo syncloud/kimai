23a756369eb3f6c66b67fd778b845867c682ec26
Boris Rybalkin <ribalkin@gmail.com>
2025-10-29 16:24:50 +0000

    next

--- test/ui.py
+++ test/ui.py
@@ -45,7 +45,7 @@ def test_login_new(selenium, device_user, device_password):
 def test_welcome(selenium):
     selenium.find_by(By.XPATH, "//a[.='Next']").click()
     selenium.find_by(By.XPATH, "//h1[.='Your profile']")
-    selenium.find_by(By.XPATH, "//a[.='Next']").click()
+    selenium.find_by(By.XPATH, "//button[.='Next']").click()
     selenium.screenshot('welcome')
 
 
